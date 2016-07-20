c17b49c6148b4ba4c0f305135a38d022318f371c
Angelos Chalaris <chalarangelo@gmail.com>
2016-07-20 20:41:58 +0300

    Collapse Button

--- js/bootstrap-extend.js
+++ js/bootstrap-extend.js
@@ -3,7 +3,7 @@
 //  Password Toggle Click Event
 $(document).on('click','.pwd-toggle, .pwd-untoggle', function(e){
   e && e.preventDefault();
-  var $field = $(this).siblings('.pwd');
+  var $field = $(e.target).siblings('.pwd');
   if ($field.attr('type') == 'password') {
       $(e.target).removeClass('pwd-toggle').addClass('pwd-untoggle');
       $field.attr('type','text');
@@ -18,4 +18,20 @@ $(document).on('click','[type="submit"]', function(e){
   $('.pwd').attr("type","password");
 });
 //      End of Password Toggle
+//=====================================================================
+//      Collapse Button
+//  Collapse Button Click Event
+$(document).on('click','.btn.btn-toggle, .btn.btn-untoggle', function(e){
+  e && e.preventDefault();
+  if($(e.target).attr('aria-expanded') == 'true')
+    $(e.target).removeClass('btn-toggle').addClass('btn-untoggle');
+  else
+    $(e.target).removeClass('btn-untoggle').addClass('btn-toggle');
+});
+//  Collapse Button's label Click Event
+$(document).on('click','.btn.btn-toggle+label, .btn.btn-untoggle+label', function(e){
+  e && e.preventDefault();
+  $(e.target).prev().click();
+});
+//    End of Collapse Button
 //=====================================================================
